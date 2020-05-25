11df66e6baeaef37d7beae69f0e5117bbc3ff663
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-25 11:39:44 +0200

    Add basic theming constants

---
+++ theme.js
@@ -0,0 +1,16 @@
+const Theme = {
+  primary: {
+    color: '#021aee',
+    fontColor: '#ffffff',
+  },
+  secondary: {
+    color: '#a200e0',
+    fontColor: '#ffffff',
+  },
+  warn: {
+    color: '#e54304',
+    fontColor: '#ffffff',
+  },
+};
+
+export default Theme;
